628bf1fd865400ec7a5b4eb70b09b1cc1fd746c5
Luiz Felipe Santana Dos Santos <fs07santana@gmail.com>
2024-02-24 18:43:08 -0300

    storage persist, remove product

--- nlw-expert-rn/src/stores/helpers/cart-in-memory.ts
+++ nlw-expert-rn/src/stores/helpers/cart-in-memory.ts
@@ -1,15 +1,27 @@
 import { ProductProps } from "@/utils/data/products";
 import { ProductCartProps } from "../cart-store";
 
-export function add(products: ProductCartProps[], newProduct: ProductProps){
-    const existingProduct = products.find(({id})=> newProduct.id === id)
+export function add(products: ProductCartProps[], newProduct: ProductProps) {
+    const existingProduct = products.find(({ id }) => newProduct.id === id)
 
-    if(existingProduct){
-        return products.map((prod)=> prod.id === existingProduct.id
-            ? {...prod, quantity: prod.quantity + 1}
+    if (existingProduct) {
+        return products.map((prod) => prod.id === existingProduct.id
+            ? { ...prod, quantity: prod.quantity + 1 }
             : prod
-            )
+        )
     }
 
-    return [...products, {...newProduct, quantity: 1}]
-} 
+    return [...products, { ...newProduct, quantity: 1 }]
+}
+
+export function remove(products: ProductCartProps[], productRemoveId: string) {
+    const updateProducts = products.map((product) =>
+        product.id === productRemoveId
+            ? {
+                ...product,
+                quantity: product.quantity > 1 ? product.quantity - 1 : 0
+            } : product
+     )
+
+    return updateProducts.filter((product) => product.quantity = 0)
+}
